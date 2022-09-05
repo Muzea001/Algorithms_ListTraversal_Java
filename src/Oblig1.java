16cef78aa01592e8dd5d490131ba21e55c43981d
Muzea001 <89633284+Muzea001@users.noreply.github.com>
2022-09-05 14:05:17 +0200

    laget metode med kodekommentar for å finne alle elementer som ikke er repeterende i et array. brukte to hjelpevariabler for å lage antall ganger et element repeteres. og for å lagre antall elementer som repeteres engang.

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -34,7 +34,7 @@ public class Oblig1 {
         }
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = 1; j < a.length; j++) {
-                // hver gang ombyttingen skjer øker vi hjelpevariabelen med 1/*
+                // hver gang ombyttingen skjer øker vi hjelpevariabelen med 1
                 if (a[i] < a[j]) {
                     teller++;
                 }
@@ -46,30 +46,62 @@ public class Oblig1 {
 
     }
 
-    //Oppgave 2 /*
+    //Oppgave 2
 
 
     public static int antallUlikeSortert(int[] a) {
         int teller = 0;
-        if (!erSortert(a)){
+        if (!erSortert(a)) {
             throw new IllegalStateException("tabellen er ikke sortert!");
         }
-        for (int i = 0; i < a.length-1; i++) {
-            //sammenligner indeks[0] med indeksen etter for å finne ut om tallet gjentas eller ikke/*
-            if(a[i]!=a[i+1]){
-                //lager et teller variabel som lagrer antall ganger tallene ikke er like/*
+        for (int i = 0; i < a.length - 1; i++) {
+            //sammenligner indeks[0] med indeksen etter for å finne ut om tallet gjentas eller ikke
+            if (a[i] != a[i + 1]) {
+                //lager et teller variabel som lagrer antall ganger tallene ikke er like
                 teller++;
             }
         }
         return teller;
     }
 
-   public static boolean erSortert(int[]a){
-        for (int i = 0; i <a.length-1 ; i++) {
-            if(a[i]>a[i+1]){
+    public static boolean erSortert(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
                 return false;
             }
         }
         return true;
     }
-}
+
+    public static int antallUlikeUsortert(int[] a) {
+
+        int teller = 0;
+        for (int i = 0; i < a.length; i++) {
+            int tall = a[i];
+            int antall = 0;
+            // bruker to hjelpevariabler, den ene er for å telle antall ganger et element finnes i arrayet, dersom elementet finnes bare en gang i arrayet øker telleren med en.
+            for (int j = 0; j < a.length; j++) {
+                if (a[j] == tall) {
+                    antall++;
+
+                }
+
+            }
+            // sjekker om elementet finnes bare en gang, hvis den gjør så øker telleren med 1.
+            if (antall == 1) {
+                teller++;
+            }
+
+        }
+        return teller;
+    }
+
+
+
+        public static void main (String[]args){
+            int array[] = {1,2,3,3,2,1,5,6,1,7,8};
+            System.out.println(antallUlikeUsortert(array));
+        }
+    }
+
+
