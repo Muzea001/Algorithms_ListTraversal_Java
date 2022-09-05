f2204e654da067d6d1bd940c00b399837fbe5bf1
Muzea001 <89633284+Muzea001@users.noreply.github.com>
2022-09-05 20:45:04 +0200

    Laget metode for å rotere alle tall i en plass til høyere. lagrer siste index og setter den som nullte index i det nye arrayet.

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -152,9 +152,23 @@ public class Oblig1 {
         }
     }
 
+    public static void rotasjon2(char[]a,int k){
+        for (int i = 0; i <k ; i++) {
+        char siste;
+            siste = a[a.length - 1];
+            for (int j = a.length-1; j > 0; j++) {
+                a[j] = a[j - 1];
+            }
+            a[k - 1] = siste;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
     public static void main(String[] args) {
         char array[] = {'a','b','c','d','e','f'};
-        rotasjon(array);
+        rotasjon2(array,3);
     }
 }
 
